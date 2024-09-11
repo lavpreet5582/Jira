fe918af48818a37bc4d19b542a0e553a4e7585df
Lavpreet Singh <lsv2885655@gmail.com>
2024-09-11 23:18:14 +0530

    ADD: Added Postgresql Database settings.

--- jira/settings.py
+++ jira/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'jira.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jira_db',
+        'USER': 'jira_admin',
+        'PASSWORD': 'jira@123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
