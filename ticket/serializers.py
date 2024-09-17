e74a8daac4c2515536b0e4d138abc004bddb0a0d
Lavpreet Singh <lavpreet.singh@renewbuy.com>
2024-09-17 10:42:48 +0530

    ADD: Added user authentication in jira app.

--- ticket/serializers.py
+++ ticket/serializers.py
@@ -4,6 +4,7 @@ from rest_framework import serializers
 
 class TicketSerializer(serializers.ModelSerializer):
     user_email = serializers.ReadOnlyField(source="user.email")
+    user_name = serializers.ReadOnlyField(source="user.first_name")
     class Meta:
         model = Ticket
-        fields = ["id", "user_email", "title", "body", "created_at"]
+        fields = ["id", "user_email", "user_name", "title", "body", "created_at"]
